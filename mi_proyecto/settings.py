1d38090ac1f691686b1d89860fa408b322f6f570
joaco <carnelosjoaquin@gmail.com>
2025-06-28 19:21:16 -0300

    test  CSRF

--- mi_proyecto/settings.py
+++ mi_proyecto/settings.py
@@ -154,17 +154,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'app', 'static'),
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "https://petmatchreact.com",
-    # ...otros...
-]
-CSRF_TRUSTED_ORIGINS = [
-    "https://petmatchreact.com",
-    # ...otros...
-]
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
