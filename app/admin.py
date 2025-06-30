9a98d66c2349d80cbae6e1150d6bf342df3e4797
joaco <carnelosjoaquin@gmail.com>
2025-06-30 10:11:15 -0300

    estilos home carrousel, formulario, chat,perfil

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Mascota, SolicitudMascota, TraspasoMascota, MensajeChat
 
-# Register your models here.
+class MascotaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'duenio', 'estado')
+    search_fields = ('nombre', 'duenio__username')
+
+admin.site.register(Mascota, MascotaAdmin)
+admin.site.register(SolicitudMascota)
+admin.site.register(TraspasoMascota)
+admin.site.register(MensajeChat)
